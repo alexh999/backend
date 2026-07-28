4a3f7d1b5078493faaf3c61824d09ed70e14954a
Hollis2005 <1465105753@qq.com>
2026-07-28 14:34:57 +0800

    register quant router

--- app/api/router.py
+++ app/api/router.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter
 
 from app.api.health import router as health_router
+from app.modules.quant.router import router as quant_router
 
 api_router = APIRouter()
 api_router.include_router(health_router)
+api_router.include_router(quant_router)
